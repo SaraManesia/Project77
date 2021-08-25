281f5928dc4d3e4ec6913d9ec5366243cc591f4a
SaraManesia <89428577+SaraManesia@users.noreply.github.com>
2021-08-25 16:58:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My First Console Project");
+var first_name= "Sara";
+var last_name= "Manesia";
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+var a=20;
+console.log(a);
+var b=30;
+console.log(b);
+var add=a+b;
+console.log(add);
+var multi=a*b;
+console.log(multi);
